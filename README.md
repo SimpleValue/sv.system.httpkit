64de86c92d3bd11a05286e851b65476fb7192eef
Maximilian Weber <weber.maximilian@googlemail.com>
2016-06-12 17:59:59 +0200

    Extracted sv.system.httpkit from sv/system.

---
+++ README.md
@@ -0,0 +1,11 @@
+# system.httpkit
+
+Components and helper functions to use [HTTP
+Kit](http://www.http-kit.org/) in a sv/system-based application.
+
+## License
+
+Copyright © 2016 Max Weber (SimpleValue)
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject sv/system.httpkit "0.1.0-SNAPSHOT"
+  :description "Components and helper functions to use HTTP Kit in a
+                sv/system-based application."
+  :url "https://github.com/SimpleValue/sv.system.httpkit"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [http-kit "2.1.18"]])
